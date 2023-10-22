1dd563c382ecf08777c41d37a723bd4484632633
Sadheera Madhushan <gitzdsproj@gmail.com>
2023-10-23 01:30:53 +0530

    branch-012: twelth commit

--- src/main/java/com/example/projectawsdeploy/ProjectAwsDeployApplication.java
+++ src/main/java/com/example/projectawsdeploy/ProjectAwsDeployApplication.java
@@ -2,17 +2,9 @@ package com.example.projectawsdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectAwsDeployApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ProjectAwsDeployApplication.class);
-    }
-
+public class ProjectAwsDeployApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectAwsDeployApplication.class, args);
     }
