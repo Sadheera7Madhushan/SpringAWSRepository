01d03f0c917e106f9dae6e92b87346f31b18d582
Sadheera Madhushan <gitzdsproj@gmail.com>
2023-11-01 00:20:55 +0530

    branch-021: 21-commit

--- src/main/java/com/example/projectawsdeploy/controllers/UserController.java
+++ src/main/java/com/example/projectawsdeploy/controllers/UserController.java
@@ -3,10 +3,17 @@ package com.example.projectawsdeploy.controllers;
 import com.example.projectawsdeploy.configurations.UserService;
 import com.example.projectawsdeploy.dto.request.RegisterDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Objects;
@@ -49,9 +56,36 @@ public class UserController {
     }
 
     @GetMapping("/home")
-    public String home(RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("successMessage", "welcome");
+    public String home(Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username;
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof UserDetails) {
+                username = ((UserDetails) principal).getUsername();
+            } else {
+                String errorMessage = "User is not authenticated or has no UserDetails";
+                log.error("User is not authenticated or has no UserDetails");
+                return String.valueOf(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage));
+            }
+        } else {
+            String errorMessage = "No authentication found";
+            log.error("No authentication found");
+            return String.valueOf(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage));
+        }
+
+        log.info("current user is: " + username);
+        model.addAttribute("successMessage", "You have been logged successfully");
+        model.addAttribute("user", username);
         return "home";
+    }
+
+    @GetMapping("/section1")
+    public String section1(Model model) {
+        return "section1";
     }
 
 }
